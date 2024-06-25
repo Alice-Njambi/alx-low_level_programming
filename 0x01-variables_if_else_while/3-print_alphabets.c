aea0b12c02e46968e7facd81407a5679ebe81a64
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-24 18:08:20 -0700

    Task 3 - ommitted whitespace trails, and added a description.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-
+/**
+* main - entry point.
+*
+* Description: A program that prints the alphabets in lowercase, then in uppercase.
+* Return: Zero,0.
+*/
 int main(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
 
-    for (letter = 'A'; letter <= 'Z'; letter++)
-    {
-        putchar(letter);
-    }
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
 
-    putchar('\n'); 
+putchar('\n');
 
-    return 0;
+return 0;
 }
