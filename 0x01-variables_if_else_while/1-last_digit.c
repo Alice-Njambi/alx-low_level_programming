9e99b01d61c50bd3504f2dbf4a11c33c73d12e3e
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-24 17:25:24 -0700

    Generating negative numbers.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,11 @@ int main(void)
 int n, last_digit;
 
 srand(time(0));
-n = rand();
+n = rand() - RAND_MAX / 2;
 
-last_digit = n % 10;
+last_digit = abs(n % 10);
+if (n < 0)
+	last_digit = -last_digit;
 
 printf("Last digit of %d is ", n);
 
