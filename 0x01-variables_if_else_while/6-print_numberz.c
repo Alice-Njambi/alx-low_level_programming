3dc6ca4e1b100454cb2873f9f0c499bcc4231e1f
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-24 18:41:30 -0700

    Task 6 - polishing errors and output.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,12 @@
 * Description: Program that prints all base 10 single digit numbers.
 * Return: Zero,0.
 */
-int main()
+int main(void)
 {
 int i = 0;
-while (i < 10) {
+while (i < 10)
+{
 putchar(i + '0');
-putchar('\n');
 i++;
 }
 
