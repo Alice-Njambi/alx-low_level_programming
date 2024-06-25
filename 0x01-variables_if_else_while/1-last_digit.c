524d3aa60e2142e7d25d1b9b9d0ab9c5570ef2ed
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-24 17:13:31 -0700

    Ommitted spaces at the start of the line.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int n, last_digit;
+int n, last_digit;
 
-    srand(time(0));
-    n = rand();
+srand(time(0));
+n = rand();
 
-    last_digit = n % 10;
+last_digit = n % 10;
 
-    printf("Last digit of %d is ", n);
+printf("Last digit of %d is ", n);
 
-    if (last_digit > 5)
-        printf("%d and is greater than 5\n", last_digit);
-    else if (last_digit == 0)
-        printf("%d and is 0\n", last_digit);
-    else
-        printf("%d and is less than 6 and not 0\n", last_digit);
+if (last_digit > 5)
+printf("%d and is greater than 5\n", last_digit);
+else if (last_digit == 0)
+printf("%d and is 0\n", last_digit);
+else
+printf("%d and is less than 6 and not 0\n", last_digit);
 
-    return (0);
+return (0);
 }
