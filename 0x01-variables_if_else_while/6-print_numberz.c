27456776dabaa3dd7728fa93f7843718ea80d489
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-24 18:38:57 -0700

    Task 6 - Using not more than 2 putchars correction.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,21 +5,15 @@
 * Description: Program that prints all base 10 single digit numbers.
 * Return: Zero,0.
 */
-int main() {
+int main()
+{
 int i = 0;
 while (i < 10) {
-	if (i < 9)
-	{
-		putchar(i + '0');
-		putchar('\n');
-	}
-	else
-	{
-		putchar('9');
-		putchar('\n');
-	}
+putchar(i + '0');
+putchar('\n');
 i++;
 }
+
 return (0);
 }
 
