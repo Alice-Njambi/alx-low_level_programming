224a252a1c820c4b2c1fc55139988e5028fc3704
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-26 14:49:38 -0700

    Added a header file, the main execution file and defintion file.

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "math.h"
+
+void print_putchar(void);
+/**
+ * main - entry point.
+ * print_putchar - function prints string of characters.
+ *
+ * Description:  Program prints "_putchar" on screen
+ * Return: Zero, 0.
+ */
+int main(void)
+{
+print_putchar();
+return 0;
+}
