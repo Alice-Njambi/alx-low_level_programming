2eaf8971b749a571f959d1576874c29f0253f976
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-30 22:28:16 -0700

    Battery St.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include"main.h"
 
 /**
- *reset_to_98-Updatesthevalueofanintegerto98.
- *@n:Apointertoaninteger.
+ *reset_to_98-Updates the value of aninteger to 98.
+ *@n:A pointer to an integer.
  *
  *Return:void
  */
-void reset_to_98(int*n)
+void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
