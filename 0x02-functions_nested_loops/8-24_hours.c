3afb45ce72ba84c20ed67c49dd2724e3948a74bd
Alice-Njambi <alice.njambi04@gmail.com>
2024-06-30 21:32:39 -0700

    Clearing spaces at the bginning of the line.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
- *
- * Return : Void.
- */
+* jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+*
+* Return : Void.
+*/
 
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            printf("%02d:%02d\n", hour, minute);
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
 }
