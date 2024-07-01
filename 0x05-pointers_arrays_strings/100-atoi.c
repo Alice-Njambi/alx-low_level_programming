c5c14dde5b2beef25d27e3f6f864a71481d4e7d5
Alice-Njambi <alice.njambi04@gmail.com>
2024-07-01 13:44:38 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _atoi - converts a string to an integer.
+* @s: input string.
+*
+* Return: integer representation of the string.
+*/
+int _atoi(char *s)
+{
+int sign = 1;
+int result = 0;
+int started = 0;
+
+while (*s)
+{
+if (*s == '-')
+{
+sign = -sign;
+}
+else if (*s >= '0' && *s <= '9')
+{
+result = result * 10 + (*s - '0');
+started = 1;
+}
+else if (started)
+{
+break;
+}
+s++;
+}
+
+return (result * sign);
+}
