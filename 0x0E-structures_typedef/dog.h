430a9c042e9a01406af5adcdeec0f3b7943c35f1
Alice-Njambi <alice.njambi04@gmail.com>
2024-07-31 11:48:07 -0700

    A new type struct dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic info
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: This structure holds the basic information about a dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
