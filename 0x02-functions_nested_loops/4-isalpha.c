f45127e20ad1c1c2debe72665b90a67f8df147dc
Alice-Njambi <alice.njambi04@gmail.com>
2024-08-28 13:08:30 -0700

    A  function that prints the sign of a number.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if a character is alphabetic (uppercase or lowercase)
- * @c: The character to check
- *
- * Return: 1 if c is a letter, 0 otherwise
- */
+* _isalpha - checks if a character is alphabetic (uppercase or lowercase)
+* @c: The character to check
+*
+* Return: 1 if c is a letter, 0 otherwise
+*/
 int _isalpha(int c)
 {
-    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
