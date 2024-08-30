b62dafe2dee86fbcb03b0e3b0d760e0485cf95b5
Alice-Njambi <alice.njambi04@gmail.com>
2024-08-30 09:02:18 -0700

    Diego.

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,17 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * positive_or_negative - the main function of the program
- * @i: the function receives no arguments
- *
- * Return: no return value
- */
+* positive_or_negative - the main function of the program
+* @i: the function receives no arguments
+*
+* Return: no return value
+*/
 void positive_or_negative(int i)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is zero\n", i);
 }
